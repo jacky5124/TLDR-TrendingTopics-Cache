d381c8d1543e676f6f2e2a64f2a9e0d86919ae1f
Qiye Chen <jacky5124@gmail.com>
2024-10-12 22:57:45 -0700

    Replace tab characters with 4 spaces

--- src/main/java/com/jackist/tldr/trendingtopics/summarycache/TldrTrendingTopicsSummaryCacheApplication.java
+++ src/main/java/com/jackist/tldr/trendingtopics/summarycache/TldrTrendingTopicsSummaryCacheApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TldrTrendingTopicsSummaryCacheApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TldrTrendingTopicsSummaryCacheApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TldrTrendingTopicsSummaryCacheApplication.class, args);
+    }
 
 }
